cc17f2ada16e5bb83b440b9c39b32f95d0e6bd9d
koki-algebra <yotsuba00040@gmail.com>
2024-02-27 14:26:02 +0900

    fix csv import

--- docker/sql/1_create_table.sql
+++ docker/sql/1_create_table.sql
@@ -5,9 +5,3 @@ CREATE TABLE IF NOT EXISTS users (
 	name TEXT NOT NULL,
 	PRIMARY KEY (id)
 );
-
-CREATE TABLE IF NOT EXISTS groups (
-	id UUID DEFAULT uuid_generate_v4(),
-	name TEXT NOT NULL,
-	PRIMARY KEY (id)
-);
